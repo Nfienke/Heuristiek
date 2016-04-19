bcadf4e7a033ca0b846c6dd282b996b1808dc2ab
Nfienke <nienkepot@hotmail.com>
2016-04-19 13:03:37 +0200

    update

---
+++ tegelset.py
@@ -0,0 +1,25 @@
+class Canvas:
+"""
+The canvas represents the empty space
+that needs to be filled with the tiles.
+"""
+    def FreeSpace:
+    """
+    freespace - usedspace
+    """
+
+    def UsedSpace:
+    """
+    the
+
+    """
+
+class Tileset:
+    """
+    """
+    tileset = [] #alle tegels in het probleem
+
+    def Tile:
+    """
+    Width, height
+    """
