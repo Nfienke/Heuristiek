d21dd069e4fd742e3027d1d7300470d6ea84f148
Nfienke <nienkepot@hotmail.com>
2016-04-24 19:11:59 +0200

    update

--- Python tegel/tegelset.py
+++ Python tegel/tegelset.py
@@ -1,32 +1,44 @@
 from tileSets import *
 
-class Canvas:
-"""
-The canvas represents the empty space
-that needs to be filled with the tiles.
-"""
-    def FreeSpace:
+
+
+class Canvas(object):
     """
-    freespace - usedspace
+    The canvas represents the empty space
+    that needs to be filled with the tiles.
     """
 
-    def UsedSpace:
-    """
-    the
+    def __init__ (self, width, height):
+        """
+        the
+        """
+        space = []
+        row=[]
+        for i in range(0,width):
 
-    """
+            row.append(0)
+            for i in range(0, height):
+                space.append(row)
+
+        #prints the canvas with freespace(0)
+        for row in space:
+            print row
+
+x = Canvas(17,17)
+
+
 
 class Tileset:
     """
     """
-    
+
     # Sorteren groot naar klein.
     sortTileSet = []
 
     #for i in sortTileSet:
         #past ie?
 
-    def Tile:
-    """
-    Width, height
-    """
+    def Tile(self, width, height, name):
+        """
+        Width, height
+        """
