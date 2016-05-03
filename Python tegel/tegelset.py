a2a9b0f0848e3154d286ff56432acfa953bb1fda
rubenpostma <ruben.postma@student.uva.nl>
2016-05-03 12:31:42 +0200

    update

--- Python tegel/tegelset.py
+++ Python tegel/tegelset.py
@@ -12,38 +12,44 @@ class Canvas():
 
     def __init__ (self, width, height):
         """
-        the
+        creates the canvas
         """
         self.widthCanvas = width
         self.heightCanvas = height
-
 
         # maak coordinaten door list van list te maken
         self.space = [[0 for count in range(width)] for count in range(height)]
 
     def placeTile(self, tileName, tileHeight, tileWidth):
-        # zoek de volgende positie
+        tiles = Tile
+        # zoek de volgende positie van de tegel
         start = self.findNextPosition()
-        
+
         # geef x en y coordinaat om tegel neer te zetten
         startX = start[0]
         startY = start[1]
 
-        # ga af of de tegel past op elke coordinaat 
+        # ga af of de tegel past op elke coordinaat
         for i in range(tileHeight):
             for j in range (tileWidth):
                 if self.space[startY + i][startX + j] != 0 or tileWidth + startX > self.widthCanvas or tileHeight + startY > self.heightCanvas:
                     print "past niet"
+                    break
                     return False
 
-                # zet tegel neer
+                # als de tegel past wordt hij de tegel neergezet.
                 self.space[startY + i][startX + j] = tileName
-                 
+                
+                
 
-        # print canvas
+                #verwijder de tegel uit de lijst met opties
+                #loop weer door de lijst met tegels van groot naar klein.
+        # print canvas (aparte functie worden --> visualize canvas).
         for row in self.space:
             print row
         print '\n'
+        tiles.sortTileSet.pop(tiles.index)
+        print tiles.sortTileSet
 
         #updates position of tile in tile class
 
@@ -58,7 +64,7 @@ class Canvas():
             else
                 go back to try next position
         """
-        
+
         # ga de canvas af
         for i in range(self.heightCanvas):
             for j in range(self.widthCanvas):
@@ -77,27 +83,31 @@ class Tile(object):
         self.tileHeight = tile[2]
         self.tileWidth = tile[1]
         self.tileName = tile[0]
-        
-        #aanroepen placetile functie
-        #self.canvas = Canvas(17,17)
-        
-        #print self.tileName, self.tileWidth, self.tileHeight
 
+        #sorteren van groot naar klein.
+    sortTileSet = sorted(tileSet1, key=lambda x: x[1],  reverse=True)
+    
+    index = 0
     #begin positie van de tegel in Canvas.
     YPosition = []
     XPosition = []
 
 def runTileSetter():
 
-    #sorteren van groot naar klein.
-    sortTileSet = sorted(tileSet1, key=lambda x: x[1],  reverse=True)
+    tiles = Tile
+
+    print tiles.sortTileSet
 
     canvas = Canvas(17,17)
     #Loopt door de tileset.
-    for tile in sortTileSet:
-        #geeft elke tile eigenschappen
-        t = Tile(tile)
-        # zet tegel in canvas
-        canvas.placeTile(t.tileName, t.tileHeight, t.tileWidth)
-
+    
+    while tiles.sortTileSet: 
+        tiles.index = 0
+        for tile in tiles.sortTileSet:
+            #geeft elke tile eigenschappen
+            t = Tile(tile)
+            # zet tegel in canvas
+            canvas.placeTile(t.tileName, t.tileHeight, t.tileWidth)
+            tiles.index += 1
+            print tile
 runTileSetter()
