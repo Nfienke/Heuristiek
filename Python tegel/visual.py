3a20cc513ec9518a609e4ed4438bd2138ae113cb
Nfienke <nienkepot@hotmail.com>
2016-05-09 21:59:21 +0200

    nu een beetje netter en met comments

--- Python tegel/visual.py
+++ Python tegel/visual.py
@@ -3,48 +3,44 @@
 
 import Tkinter
 import tkMessageBox
-from tegelset import *
-
-#http://stackoverflow.com/questions/22950997/random-fill-colour-for-shapes-in-pythontkinter
 import random
+from tegelset import *
 
 top = Tkinter.Tk()
 
-print "coordinaten", placedCoordinates
-print "tileset", tileSet
-#alles keer 30?
-C = Tkinter.Canvas(top, height=17*30, width=17*30)
+#vergrotingsfactor
+f = 30
+C = Tkinter.Canvas(top, height=17*f, width=17*f)
 
 X0list = []
 Y1list = []
 index = 0
 
-
+#loopt door alle coordinaten van de geplaatste tegels voor het begin coordinaat,
+#en voor elke tegel worden de coordinaten gegeven voor de rectangle.
 for tile in placedCoordinates:
 
     tileName = tile[0]
-    X0 = tile[1]*30
-    Y1 = tile[2]*30
+    #X0 is het begin coordinaat X.
+    X0 = tile[1]*f
+    #Y0 is het begin coordinaat Y.
+    Y1 = tile[2]*f
 
-    X0list.append(X0)
-    Y1list.append(Y1)
-
-    index += 1
+    #Ook moet voor elke tegel de afmetingen bekend zijn, verkregen uit tileSet.
     for tile2 in tileSet:
 
         if tileName == tile2[0]:
 
-            if index > 1:
-                Y0 = Y1list[index-1] + tile2[2]*30
-                X1 = X0list[index-1] + tile2[1]*30
+            #Y0 = Y1 + hoogte van de tegel,
+            #en X1 = X0 + breedte van de tegel.
+            Y0 = Y1 + tile2[2]*f
+            X1 = X0 + tile2[1]*f
 
-            else:
-                Y0 = tile2[2]*30
-                X1 = tile2[1]*30
-
+            #Coordinaten van de rectangle(tegel).
             coor = X0, Y0, X1, Y1
 
             #http://stackoverflow.com/questions/22950997/random-fill-colour-for-shapes-in-pythontkinter
+            #tekent elke tegel in het canvas met een random color.
             tileName = C.create_rectangle(coor, fill= "#"+("%06x"%random.randint(0,16777215)), width=2)
 
 C.pack()
